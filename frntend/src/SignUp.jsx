823354e0dddb8d3d5592ebc5462632dc804970d1
doja117 <1899doja@gmail.com>
2023-11-14 00:03:20 +0530

    test

---
+++ frntend/src/SignUp.jsx
@@ -0,0 +1,10 @@
+export default function SignUp(){
+    return (
+        <div>
+            <h1>SignUp</h1>
+            email  -    <input></input><br />
+            password  -    <input></input><br />
+            <button>Submit</button>
+        </div>
+    )
+}
